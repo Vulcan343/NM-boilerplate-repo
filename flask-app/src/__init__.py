6f6a45fdc2d93bd2863ee42fb46f8d7fdfaa2e7d
Vulcan 343 <sanpietro525@gmail.com>
2023-04-18 13:14:11 -0400

    Changes to be committed: 	modified:   flask-app/src/__init__.py 	new file:   flask-app/src/employee/employee.py

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -34,11 +34,13 @@ def create_app():
     from src.customers.customers import customers
     from src.products.products  import products
     from src.inventory.inventory  import inventory
+    from src.employee.employee  import employee
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
     app.register_blueprint(customers,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(inventory,    url_prefix='/i')
+    app.register_blueprint(employee,    url_prefix='/e')
 
     return app
