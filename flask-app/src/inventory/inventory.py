951b6c915d98c469e99ba2a66217d6e346371ec8
Vulcan 343 <sanpietro525@gmail.com>
2023-04-18 13:22:08 -0400

    Changes to be committed: 	modified:   flask-app/src/inventory/inventory.py
    added a route to the inventory blueprint

--- flask-app/src/inventory/inventory.py
+++ flask-app/src/inventory/inventory.py
@@ -4,3 +4,18 @@ from src import db
 
 
 inventory = Blueprint('inventory', __name__)
+
+# route to get the entire stock of the company
+@inventory.route('/inventory', methods=['GET'])
+def get_inventory():
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from Inventory')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
